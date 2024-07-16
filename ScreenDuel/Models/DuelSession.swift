ac8c9021d7860d53c71c7a6c8fa06d47e234e89c
liongoogle <113564377+liongoogle@users.noreply.github.com>
2024-07-16 19:26:25 -0400

    created basic time selection and app selection views branching from home page

---
+++ ScreenDuel/Models/DuelSession.swift
@@ -0,0 +1,8 @@
+//
+//  DuelSession.swift
+//  ScreenDuel
+//
+//  Created by Leo Howerdd on 7/8/24.
+//
+
+import Foundation
---
+++ ScreenDuel/Views/AppSelectorView.swift
@@ -0,0 +1,8 @@
+//
+//  AppSelectorView.swift
+//  ScreenDuel
+//
+//  Created by Leo Howerdd on 7/8/24.
+//
+
+import Foundation
